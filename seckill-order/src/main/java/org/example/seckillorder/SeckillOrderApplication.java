2a1c53f8b56791ab04f8e6cf980596755d42427e
33-ruby <2921354564@qq.com>
2026-04-19 13:22:10 +0800

    服务注册发现与配置
    搭建 Nacos
    各服务注册到 Nacos
    结合 Spring Cloud Gateway 网关
    动态配置管理
    
    Signed-off-by: 33-ruby <2921354564@qq.com>

--- seckill-order/src/main/java/org/example/seckillorder/SeckillOrderApplication.java
+++ seckill-order/src/main/java/org/example/seckillorder/SeckillOrderApplication.java
@@ -1,9 +1,13 @@
 package org.example.seckillorder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("org.example.seckillorder.mapper")
 public class SeckillOrderApplication {
 
     public static void main(String[] args) {
--- seckill-stock/src/main/java/org/example/stock/StockApplication.java
+++ seckill-stock/src/main/java/org/example/stock/StockApplication.java
@@ -3,8 +3,10 @@ package org.example.stock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("org.example.stock.mapper")
 public class StockApplication {
     public static void main(String[] args) {
